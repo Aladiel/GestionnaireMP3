f4ae030fb965d5aacbbc68d900d75d30754a9577
DAGASHY\zseba <zsebastien@yahoo.fr>
2025-05-19 16:24:37 +0200

    add artist-class, sécurité du type de données

---
+++ app/models/Artist.py
@@ -0,0 +1,15 @@
+class Artist:
+
+    def __init__(self, name_artist: str):
+        self.name_artist = name_artist
+
+    @property
+    def name_artist(self) -> str:
+        return self.__name_artist
+
+    @name_artist.setter
+    def name_artist(self, value):
+        if not isinstance(value, str):
+            raise TypeError("name_artist must be a string")
+
+        self.__name_artist = value.strip().lower()
